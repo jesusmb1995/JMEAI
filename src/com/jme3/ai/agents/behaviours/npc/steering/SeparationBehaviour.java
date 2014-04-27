524bba7d987d8ad6164d487285b04993b4a73edb
Jesús <jesus.mb.1995@gmail.com>
2014-04-28 00:10:00 +0200

    Update SeparationBehaviour.java

--- src/com/jme3/ai/agents/behaviours/npc/steering/SeparationBehaviour.java
+++ src/com/jme3/ai/agents/behaviours/npc/steering/SeparationBehaviour.java
@@ -2,30 +2,7 @@
 Copyright (c) 2014, Jesús Martín Berlanga
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this 
-list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software without 
-specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Distributed under the BSD licence.
 */
 
 package com.jme3.ai.agents.behaviours.npc.steering;
@@ -35,13 +12,13 @@ import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Spatial;
-import java.util.Arrays;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Jesús Martín Berlanga
- * @version 1.0
+ * @version 1.1
  */
 public class SeparationBehaviour extends AbstractSteeringBehaviour{
     
@@ -64,9 +41,9 @@ public class SeparationBehaviour extends AbstractSteeringBehaviour{
      * @param spatial active spatial during excecution of behaviour
      * @see SeparationBehaviour#SeparationBehaviour(com.jme3.ai.agents.Agent, com.jme3.ai.agents.Agent[]) 
      */
-    public SeparationBehaviour(Agent agent, Agent[] initialObstacles, Spatial spatial) {
+    public SeparationBehaviour(Agent agent, List<Agent> initialObstacles, Spatial spatial) {
         super(agent, spatial);
-        this.obstacles = Arrays.asList(initialObstacles);
+        this.obstacles = initialObstacles;
     }
     
     @Override
